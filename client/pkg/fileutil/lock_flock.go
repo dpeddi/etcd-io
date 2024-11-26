22e84c40caee31fd9823e134636e1dde50229e46
dpeddi <1086815+dpeddi@users.noreply.github.com>
2024-11-26 14:11:59 +0000

    aix support for flock

--- client/pkg/fileutil/lock_flock.go
+++ client/pkg/fileutil/lock_flock.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !windows && !plan9 && !solaris
+//go:build !windows && !plan9 && !solaris && !aix
 
 package fileutil
 
